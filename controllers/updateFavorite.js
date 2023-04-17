865b6ea044488bd4b83be6548cf6f653b4138474
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-04-17 11:35:05 +0300

    fix:200 status

--- controllers/updateFavorite.js
+++ controllers/updateFavorite.js
@@ -8,7 +8,7 @@ const updateFavorite = async (req, res) => {
   if (!result) {
     throw RequestError(404, "Not found");
   }
-  res.status(201).json(result);
+  res.status(200).json(result);
 };
 
 module.exports = updateFavorite;
