00dbc488a3739bf62a4f6f4559ebda68a4430824
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-04-22 16:37:36 +0300

    add: register user

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 const contactsRouter = require("./routes/api/contacts");
+const authRouter = require("./routes/api/auth");
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/auth", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
