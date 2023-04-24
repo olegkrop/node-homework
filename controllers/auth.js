22cdb4ac574d62d6da238355c54a052379a7b5a3
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-04-24 15:59:07 +0300

    add: password hash

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,16 @@
+const bcrypt = require("bcryptjs");
 const { User } = require("../models/user");
 const { RequestError, ctrlWrapper } = require("../helpers");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user) {
-    throw RequestError(409, " This email already in use ");
+    throw RequestError(409, "This email already in use");
   }
-  const newUser = await User.create(req.body);
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
   res.status(201).json({
     email: newUser.email,
     name: newUser.name,
