918f404678c6dfd1851b8058d01a1f02da071437
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-04-24 19:20:32 +0300

    add: pagination

--- controllers/getAll.js
+++ controllers/getAll.js
@@ -3,7 +3,13 @@ const { Contact } = require("../models/contact");
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
 
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   res.json(result);
 };
 
