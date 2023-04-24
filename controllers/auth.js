8c3aa68f2a01711913caab6f7b68153767580d31
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-04-24 15:02:24 +0300

    add: check on unique user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,12 @@
 const { User } = require("../models/user");
-const { ctrlWrapper } = require("../helpers");
+const { RequestError, ctrlWrapper } = require("../helpers");
+
 const register = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw RequestError(409, " This email already in use ");
+  }
   const newUser = await User.create(req.body);
   res.status(201).json({
     email: newUser.email,
